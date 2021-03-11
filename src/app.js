d0736bc09a952a49afb50d0947a13c5b02a43d55
Maksim Verkhoturov <sd32@protonmail.com>
2021-03-10 22:37:41 -0700

    successfully integrates socket.io w/ express

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const middlewares = require('./middlewares')
 require('dotenv').config()
 
-const port = process.env.PORT || 5005
+const PORT = process.env.PORT || 5005
 
 const app = express()
 
@@ -14,32 +14,23 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 
-const httpServer = require('http').Server(app)
-const io = require('socket.io')(httpServer, {
-  cors: {
-    origin: false,
-  },
-})
-
-app.use((req, res, next) => {
-  res.io = io
-  next()
-})
-
-io.on('connection', (socket) => {
-  console.log('headshake', socket.handshake)
-})
-
-app.get('/', function (req, res, next) {
-  console.log(res.io.emit('socketToMe', 'users'))
+app.get('/', function (req, res) {
   res.send('respond with a resource.')
 })
 
 app.use(middlewares.notFound)
 app.use(middlewares.errorHandler)
 
-app.listen(port, () => {
+const appInstance = app.listen(PORT, () => {
   /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`)
-  /* eslint-enable no-console */
+  console.log(`Listening: http://localhost:${PORT}`)
+})
+
+const io = require('socket.io')(appInstance, {
+  cors: { origin: 'http://localhost:8080' },
+})
+
+io.on('connection', function (socket) {
+  console.log('made socket connection')
+  socket.emit('bryan', 'is nice')
 })
