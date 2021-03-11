9785690f7d189821fe9ac9d263d2024ec5890d57
Maksim Verkhoturov <sd32@protonmail.com>
2021-03-11 01:16:03 -0700

    adds basic move to game-room from wait-room

--- src/socket.js
+++ src/socket.js
@@ -1,24 +1,28 @@
 const { io } = require('./index.js')
 
-const getPlayersIds = (roomName) => io.of('/').adapter.rooms.get(roomName)
-const getSocketsByIds = (socketId) => io.of('/').sockets.get(socketId)
-const getPlayersSockets = (roomName) =>
-  getPlayersIds(roomName).map((socketId) => getSocketsByIds(socketId))
+const socketIdsFromRoom = (roomName) => Array.from(io.of('/').adapter.rooms.get(roomName))
+const socketFromId = (socketId) => io.of('/').sockets.get(socketId)
+const socketInstancesFromRoom = (roomName) =>
+  socketIdsFromRoom(roomName).map((socketId) => socketFromId(socketId))
 
 io.of('/').adapter.on('join-room', (room, id) => {
   if (room === 'wait-room') {
     console.log(`heya! socket ${id} has joined room ${room}`)
+    if (socketIdsFromRoom('wait-room').length === 2) {
+      socketInstancesFromRoom('wait-room').forEach((socket) => {
+        socket.leave('wait-room')
+        socket.join('game-room-1')
+      })
+    }
+  }
 
-    console.log('ids', getPlayersIds('wait-room').values().next().value)
-    console.log('socket', getSocketsByIds(getPlayersIds('wait-room').values().next().value))
-    // if (getPlayersIds('wait-room').size === 2) {
-    //   getPlayersIds('wait-room').forEach((socketId) => {})
-    // }
+  if (room === 'game-room-1') {
+    console.log(`hey!! gaming time! socket ${id} has joined room ${room}`)
   }
 })
 
 io.of('/').on('connection', (socket) => {
-  console.log('made socket connection with id ', socket.id)
+  console.log('connected: ', socket.id)
   socket.join('wait-room')
   //   socket.emit('bryan', 'is nice')
 })
